a4eb76b1697cd805cfe489c5b9b91c628b61f3a1
Karine Liuti <kah.liuti@gmail.com>
2018-10-13 11:47:54 -0300

    Created button to generate phrases in Portuguese

--- index.android.js
+++ index.android.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { AppRegistry, View, Text, Image, TouchableOpacity } from 'react-native';
+import { AppRegistry, View, Text, Image, TouchableOpacity, Alert } from 'react-native';
 
 const styles = {
   button: {
     backgroundColor: '#538530',
-    marginTop: 10,
+    marginTop: 20,
     paddingHorizontal: 30,
     paddingVertical: 10
   },
@@ -20,14 +20,32 @@ const styles = {
   }
 };
 
+const generatePhrase = () => {
+  var value = Math.random();
+
+  value = Math.floor(value * 5);
+
+  var phrases = Array();
+  phrases[0] = 'Ela pode ser o que ela quiser, mas EleNão.';
+  phrases[1] = '"Lembre-se de onde você veio e saiba onde você quer chegar."';
+  phrases[2] = '"Domine o conhecimento para não ser ignorante."';
+  phrases[3] = '"Se você colhe o que gostaria, é pq não plantou o que deveria."';
+  phrases[4] = '"Se ficar parado resolvesse algo, poderíamos ser estátuas."';
+
+  Alert.alert(phrases[value]);
+};
+
 const App = () => {
   const { spaceContents, button, text } = styles;
 
   return (
     <View style={spaceContents}>
       <Image source={require('./imgs/logo.png')} />
-      <TouchableOpacity style={button}>
-        <Text style={text}>New Phrase</Text>
+      <TouchableOpacity 
+        style={button}
+        onPress={generatePhrase}
+      >
+        <Text style={text}>Phrase in Portuguese</Text>
       </TouchableOpacity>
     </View>
   );
